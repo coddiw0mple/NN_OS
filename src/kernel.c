4f16d0cba68ebfd1c32a32bc67244dc40b75d6f0
coddiw0mple <coddiw0mpl4@gmail.com>
2022-02-01 00:26:51 +0530

    disk read

--- src/kernel.c
+++ src/kernel.c
@@ -5,6 +5,7 @@
 #include "io/io.h"
 #include "memory/heap/kheap.h"
 #include "memory/paging/paging.h"
+#include "disk/disk.h"
 
 uint16_t* video_mem = 0;
 uint16_t terminal_y = 0;
@@ -88,21 +89,11 @@ void kernel_main()
     // Switching to kernel paging chunk
     paging_switch(paging_4gb_chunk_get_directory(kernel_chunk));
 
-    char* ptr = kzalloc(4096); 
-    paging_set(paging_4gb_chunk_get_directory(kernel_chunk), (void*)0x1000, (uint32_t)ptr | PAGING_ACCESS_FROM_ALL | PAGING_IS_PRESENT | PAGING_IS_WRITEABLE);
-
     // Finally enabling paging
     enable_paging();
 
-    char* ptr2 = (char*) 0x1000;
-    ptr2[0] = 'A';
-    ptr2[1] = 'B';
-
-    print("\n");
-    print(ptr2);
-
-    print("\n");
-    print(ptr);
+    char buf[512];
+    disk_read_sector(0, 1, buf);
 
     // Enabling global system interrupts
     enable_interrupts();
