b5817cf6f8f713ab937bf1010c8f2d8bb1a55a0b
coddiw0mple <coddiw0mpl4@gmail.com>
2022-01-31 23:01:38 +0530

    paging

--- src/kernel.c
+++ src/kernel.c
@@ -4,6 +4,7 @@
 #include "idt/idt.h"
 #include "io/io.h"
 #include "memory/heap/kheap.h"
+#include "memory/paging/paging.h"
 
 uint16_t* video_mem = 0;
 uint16_t terminal_y = 0;
@@ -67,6 +68,8 @@ void print(const char* str)
     }
 }
 
+static struct paging_4gb_chunk* kernel_chunk = 0;
+
 void kernel_main()
 {
     terminal_initialize();
@@ -78,6 +81,28 @@ void kernel_main()
 
     // Initializing the global interrupt descriptor table
     idt_init();
+
+    // Setting up paging
+    kernel_chunk = paging_new_4gb(PAGING_IS_WRITEABLE | PAGING_IS_PRESENT | PAGING_ACCESS_FROM_ALL);
+    
+    // Switching to kernel paging chunk
+    paging_switch(paging_4gb_chunk_get_directory(kernel_chunk));
+
+    char* ptr = kzalloc(4096); 
+    paging_set(paging_4gb_chunk_get_directory(kernel_chunk), (void*)0x1000, (uint32_t)ptr | PAGING_ACCESS_FROM_ALL | PAGING_IS_PRESENT | PAGING_IS_WRITEABLE);
+
+    // Finally enabling paging
+    enable_paging();
+
+    char* ptr2 = (char*) 0x1000;
+    ptr2[0] = 'A';
+    ptr2[1] = 'B';
+
+    print("\n");
+    print(ptr2);
+
+    print("\n");
+    print(ptr);
 
     // Enabling global system interrupts
     enable_interrupts();
