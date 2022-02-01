a0725546752ebddd2f2b659a222ab77927a8d184
coddiw0mple <coddiw0mpl4@gmail.com>
2022-02-01 21:04:27 +0530

    disk streamer

--- src/kernel.c
+++ src/kernel.c
@@ -8,6 +8,7 @@
 #include "disk/disk.h"
 #include "string/string.h"
 #include "fs/pparser.h"
+#include "disk/streamer.h"
 
 uint16_t* video_mem = 0;
 uint16_t terminal_y = 0;
@@ -89,8 +90,11 @@ void kernel_main()
     // Enabling global system interrupts
     enable_interrupts();
 
-    struct path_root* root_path = pathparser_parse("0:/bin/shell.exe", NULL);
 
-    if(root_path) {}
+    struct disk_stream* stream = diskstreamer_new(0);
+    diskstreamer_seek(stream, 0x201);
+    unsigned char c = 0;
+    diskstreamer_read(stream, &c, 1);
+    while(1) {}
 
 }
