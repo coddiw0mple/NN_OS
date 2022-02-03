1ecf81e24368c8fedbeb14b3629a88ec82262e05
coddiw0mple <coddiw0mpl4@gmail.com>
2022-02-04 04:33:19 +0530

    main

--- src/task/task.c
+++ src/task/task.c
@@ -5,6 +5,8 @@
 #include "../memory/heap/kheap.h"
 #include "../memory/memory.h"
 #include "../idt/idt.h"
+#include "../string/string.h"
+#include "../memory/paging/paging.h"
 
 // The current task that is running
 struct task *current_task = 0;
@@ -124,6 +126,43 @@ void task_save_state(struct task *task, struct interrupt_frame *frame)
     task->registers.esi = frame->esi;
 }
 
+int copy_string_from_task(struct task* task, void* virtual, void* phys, int max)
+{
+    if (max >= PAGING_PAGE_SIZE)
+    {
+        return -EINVARG;
+    }
+
+    int res = 0;
+    char* tmp = kzalloc(max);
+    if (!tmp)
+    {
+        res = -ENOMEM;
+        goto out;
+    }
+
+    uint32_t* task_directory = task->page_directory->directory_entry;
+    uint32_t old_entry = paging_get(task_directory, tmp);
+    paging_map(task->page_directory, tmp, tmp, PAGING_IS_WRITEABLE | PAGING_IS_PRESENT | PAGING_ACCESS_FROM_ALL);
+    paging_switch(task->page_directory);
+    strncpy(tmp, virtual, max);
+    kernel_page();
+
+    res = paging_set(task_directory, tmp, old_entry);
+    if (res < 0)
+    {
+        res = -EIO;
+        goto out_free;
+    }
+
+    strncpy(phys, tmp, max);
+
+out_free:
+    kfree(tmp);
+out:
+    return res;
+}
+
 void task_current_save_state(struct interrupt_frame *frame)
 {
     if (!task_current())
@@ -139,6 +178,13 @@ int task_page()
 {
     user_registers();
     task_switch(current_task);
+    return 0;
+}
+
+int task_page_task(struct task* task)
+{
+    user_registers();
+    paging_switch(task->page_directory);
     return 0;
 }
 
@@ -172,3 +218,20 @@ int task_init(struct task *task, struct process *process)
 
     return 0;
 }
+
+void* task_get_stack_item(struct task* task, int index)
+{
+    void* result = 0;
+
+    uint32_t* sp_ptr = (uint32_t*) task->registers.esp;
+
+    // Switch to the given tasks page
+    task_page_task(task);
+
+    result = (void*) sp_ptr[index];
+
+    // Switch back to the kernel page
+    kernel_page();
+
+    return result;
+} 
