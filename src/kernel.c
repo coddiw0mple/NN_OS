197bf538ac97283c9c8c114a1dc323a522e2ae6a
coddiw0mple <coddiw0mpl4@gmail.com>
2022-02-04 02:54:06 +0530

    main

--- src/kernel.c
+++ src/kernel.c
@@ -5,11 +5,15 @@
 #include "io/io.h"
 #include "memory/heap/kheap.h"
 #include "memory/paging/paging.h"
+#include "memory/memory.h"
 #include "disk/disk.h"
 #include "string/string.h"
 #include "fs/pparser.h"
 #include "disk/streamer.h"
 #include "fs/file.h"
+#include "gdt/gdt.h"
+#include "task/tss.h"
+#include "config.h"
 
 uint16_t* video_mem = 0;
 uint16_t terminal_y = 0;
@@ -70,13 +74,28 @@ void panic(const char* msg)
     while(1) {}
 }
 
+struct tss tss;
+struct gdt gdt_real[NN_OS_TOTAL_GDT_SEGMENTS];
+struct gdt_structured gdt_structured[NN_OS_TOTAL_GDT_SEGMENTS] = {
+    {.base = 0x00, .limit = 0x00, .type = 0x00},                // NULL Segment
+    {.base = 0x00, .limit = 0xffffffff, .type = 0x9a},           // Kernel code segment
+    {.base = 0x00, .limit = 0xffffffff, .type = 0x92},            // Kernel data segment
+    {.base = 0x00, .limit = 0xffffffff, .type = 0xf8},              // User code segment
+    {.base = 0x00, .limit = 0xffffffff, .type = 0xf2},             // User data segment
+    {.base = (uint32_t)&tss, .limit=sizeof(tss), .type = 0xE9}      // TSS Segment
+};
+
 void kernel_main()
 {
     terminal_initialize();
     
     print("Hello, World!\nThis is my first OS :)");
 
-    panic("Error ocurred!");
+    memset(gdt_real, 0x00, sizeof(gdt_real));
+    gdt_structured_to_gdt(gdt_real, gdt_structured, NN_OS_TOTAL_GDT_SEGMENTS);
+
+    // Load the gdt
+    gdt_load(gdt_real, sizeof(gdt_real));
 
     // Initializing our heap
     kheap_init();
@@ -89,6 +108,14 @@ void kernel_main()
 
     // Initializing the global interrupt descriptor table
     idt_init();
+
+    // Setup the TSS
+    memset(&tss, 0x00, sizeof(tss));
+    tss.esp0 = 0x600000;
+    tss.ss0 = KERNEL_DATA_SELECTOR;
+
+    // Load the TSS
+    tss_load(0x28);
 
     // Setting up paging
     kernel_chunk = paging_new_4gb(PAGING_IS_WRITEABLE | PAGING_IS_PRESENT | PAGING_ACCESS_FROM_ALL);
