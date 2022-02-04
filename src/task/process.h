3e73379518690d8a8961395090658d4d66c98307
coddiw0mple <coddiw0mpl4@gmail.com>
2022-02-04 09:19:21 +0530

    main

--- src/task/process.h
+++ src/task/process.h
@@ -77,5 +77,6 @@ void* process_malloc(struct process* process, size_t size);
 void process_free(struct process* process, void* ptr);
 void process_get_arguments(struct process* process, int* argc, char*** argv);
 int process_inject_arguments(struct process* process, struct command_argument* root_argument);
+int process_terminate(struct process* process);
 
 #endif 
